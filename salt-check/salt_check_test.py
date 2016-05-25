04de36cb0d7ee291965366b888893ad00a48e5a1
William Cannon <william.cannon@gmail.com>
2016-05-25 13:03:22 -0500

    adding unit tests for SaltCheck class

--- salt-check/salt_check_test.py
+++ salt-check/salt_check_test.py
@@ -14,6 +14,108 @@ class MyClass(unittest.TestCase):
     def tearDown(self):
         pass
 
+    def test_get_state_dir_1(self):
+        val = self.mt.get_state_dir()
+        self.assertNotEqual(val, None) 
+
+    def test_get_state_search_path_list_1(self):
+        val = self.mt.get_state_search_path_list()
+        self.assertNotEqual(val, None) 
+
+    def test_show_minion_options_1(self):
+        val = self.mt.show_minion_options()
+        self.assertNotEqual(val, None) 
+
+    def test_show_minion_options_2(self):
+        val = self.mt.show_minion_options()
+        cache = val.get('cachedir', None)
+        root_dir = val.get('root_dir', None)
+        states_dir = val.get('states_dir', None)
+        environment = val.get('environment', None)
+        file_roots = val.get('file_roots', None)
+        #if cache and root_dir and states_dir and environment and file_roots:
+        if cache and root_dir and file_roots:
+            all_good = True
+        else:
+            all_good = False
+        self.assertEqual(all_good, True) 
+
+    def test_run_test_1(self):
+        mydict = {"module_and_function": "test.echo",
+                  "assertion": "assertEqual",
+                  "expected-return": "This works!",
+                  "args":"This works!" }
+        val = self.mt.run_test(mydict)
+        self.assertEqual(val, True) 
+
+    def test_run_test_2(self):
+        mydict = {"module_and_function": "invalidmod.invalidfunc",
+                  "assertion": "assertEqual",
+                  "expected-return": "This works!",
+                  "args":["This works!"] }
+        val = self.mt.run_test(mydict)
+        self.assertEqual(val, "False: Invalid test") 
+
+    def test_run_test_3(self):
+        mydict = {"module_and_function": "test.echo",
+                  "assertion": "assertEqual",
+                  "expected-rotten": "This works!",
+                  "arrgs":["This works!"] }
+        val = self.mt.run_test(mydict)
+        self.assertEqual(val, "False: Invalid test") 
+
+    def test_populate_salt_modules_list_1(self):
+        val = self.mt.populate_salt_modules_list()
+        length = len(val)
+        self.assertGreater(length, 10) 
+
+    def test_is_valid_test_1(self):
+        test_dict = {'module_and_function':'test.ping',
+                     'assertion':'assertTrue',
+                     'expected-return':'True'}
+        val = self.mt.is_valid_test(test_dict)
+        self.assertEqual(val, True) 
+
+    def test_is_valid_test_2(self):
+        test_dict = {'module_and_function':'test.ping-a-ring',
+                     'assertion':'assertTrue',
+                     'expected-return':'True'}
+        val = self.mt.is_valid_test(test_dict)
+        self.assertEqual(val, False) 
+
+    def test_is_valid_test_3(self):
+        test_dict = {'module_and_function':'toast.ping',
+                     'assertion':'assertTrue',
+                     'expected-return':'True'}
+        val = self.mt.is_valid_test(test_dict)
+        self.assertEqual(val, False) 
+
+    def test_is_valid_test_4(self):
+        test_dict = {'module_and_function':'toast.ping',
+                     'assertion':'assertAbort',
+                     'expected-return':'True'}
+        val = self.mt.is_valid_test(test_dict)
+        self.assertEqual(val, False) 
+
+    def test_is_valid_test_5(self):
+        test_dict = {'module_and_function':'toast.ping',
+                     'absorbtion':'assertTrue',
+                     'expected-return':'True'}
+        val = self.mt.is_valid_test(test_dict)
+        self.assertEqual(val, False) 
+
+    def test_is_valid_test_6(self):
+        test_dict = {'module_and_function':'toast.ping',
+                     'assertion':'assertTrue',
+                     'expected-rotunda':'True'}
+        val = self.mt.is_valid_test(test_dict)
+        self.assertEqual(val, False) 
+
+    def test_is_valid_test_7(self):
+        test_dict = {}
+        val = self.mt.is_valid_test(test_dict)
+        self.assertEqual(val, False) 
+
     def test_call_salt_command_1(self):
         val = self.mt.call_salt_command('test.ping')
         self.assertEqual(val, True) 
